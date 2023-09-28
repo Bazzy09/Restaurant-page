8566c56434cbd0a3a46fbec588d0cc9e102786ee
joshua nathaniel <ministerjoe456@gmail.com>
2023-09-28 13:02:01 +0100

    Refactor navigation and improve code structure.

--- src/contact.js
+++ src/contact.js
@@ -1,9 +1,8 @@
 function loadContactPage() {
-    const content = document.getElementById('content');
-    const contactDiv = document.createElement('div');
+  const content = document.getElementById('content');
+  const contactDiv = document.createElement('div');
     contactDiv.textContent = 'Contact us for reservations!';
     content.appendChild(contactDiv);
   }
-  
-  export default loadContactPage;
-  
+
+export default loadContactPage;
---
+++ src/website.js
@@ -0,0 +1,97 @@
+import loadHome from "./home";
+import loadMenu from "./menu";
+import loadReservation from "./reservation";
+import loadContact from "./contact";
+
+function createHeader() {
+  const header = document.createElement("header");
+  header.classList.add("header");
+
+  const restaurantName = document.createElement("h1");
+  restaurantName.classList.add("restaurant-name");
+  restaurantName.textContent = "The 5th";
+
+  header.appendChild(restaurantName);
+  header.appendChild(createNav());
+
+  return header;
+}
+
+function createNav() {
+  const nav = document.createElement("nav");
+
+  const homeButton = document.createElement("button");
+  homeButton.classList.add("button-nav");
+  homeButton.textContent = "Home";
+  homeButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(homeButton);
+    clearContent();
+    loadHome();
+  });
+
+  const menuButton = document.createElement("button");
+  menuButton.classList.add("button-nav");
+  menuButton.textContent = "Menu";
+  menuButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(menuButton);
+    loadMenu();
+  });
+
+  const reservationButton  = document.createElement("button"); 
+  reservationButton.classList.add("button-nav");
+  reservationButton.textContent = "Reservation";
+  reservationButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(reservationButton);
+    loadReservation();
+  });
+
+  const contactButton = document.createElement("button");
+  contactButton.classList.add("button-nav");
+  contactButton.textContent = "Contact";
+  contactButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(contactButton);
+    loadContact();
+  });
+
+  nav.appendChild(homeButton);
+  nav.appendChild(menuButton);
+  nav.appendChild(reservationButton);
+  nav.appendChild(contactButton);
+
+  return nav;
+}
+
+function setActiveButton(button) {
+  const buttons = document.querySelectorAll(".button-nav");
+
+  buttons.forEach((button) => {
+    if (button !== this) {
+      button.classList.remove("active");
+    }
+  });
+
+  button.classList.add("active");
+}
+
+function createMain() {
+  const main = document.createElement("main");
+  main.classList.add("main");
+  main.setAttribute("id", "main");
+  return main;
+}
+
+
+function initializeWebsite() {
+  const content = document.getElementById("content");
+
+  content.appendChild(createHeader());
+  content.appendChild(createMain());
+  setActiveButton(document.querySelector(".button-nav"));
+  loadHome();
+}
+
+export default initializeWebsite;
