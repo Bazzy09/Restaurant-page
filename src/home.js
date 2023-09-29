6b7c29e793084680043480c744393b7f619b8c1f
joshua nathaniel <ministerjoe456@gmail.com>
2023-09-29 21:17:53 +0100

    added a background image container

--- src/home.js
+++ src/home.js
@@ -1,9 +1,19 @@
 function loadHomePage() {
-    const content = document.getElementById('content');
-    const homeDiv = document.createElement('div');
-    homeDiv.textContent = 'Welcome to our wonderful restaurant!';
-    content.appendChild(homeDiv);
-  }
+  const content = document.getElementById('content');
+  const homeDiv = document.createElement('div');
+  homeDiv.classList.add('home-page'); 
+
+  const backgroundImage = document.createElement('div');
+  backgroundImage.classList.add('background-image'); 
+  backgroundImage.style.backgroundImage = 'url("path/to/your-image.jpg")'; 
+
+  homeDiv.appendChild(backgroundImage);
+
+  const welcomeMessage = document.createElement('div');
+  welcomeMessage.textContent = 'Welcome to The 5th – A Taste of Excellence';
+  homeDiv.appendChild(welcomeMessage);
   
-  export default loadHomePage;
-  
+  content.appendChild(homeDiv);
+}
+
+export default loadHomePage;
