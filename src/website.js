b8d33d8e8196946bbec892230c6579bc8d99d2d0
joshua nathaniel <ministerjoe456@gmail.com>
2023-10-03 12:14:37 +0100

    added navigation links for home, menu, reservation and contact page.

--- src/website.js
+++ src/website.js
@@ -1,83 +1,60 @@
-import loadHome from "./home";
-import loadMenu from "./menu";
-import loadReservation from "./reservation";
-import loadContact from "./contact";
-
-function createHeader() {
-  const header = document.createElement("header");
-  header.classList.add("header");
-
-  const restaurantName = document.createElement("h1");
-  restaurantName.classList.add("restaurant-name");
-  restaurantName.textContent = "The 5th Chops";
-  
-  header.appendChild(createNav());
-  header.appendChild(restaurantName);
-  
-
-  return header;
-}
-
-function createNav() {
-  const nav = document.createElement("nav");
-
-  const homeButton = createNavButton("Home", loadHome);
-  const menuButton = createNavButton("Menu", loadMenu);
-  const reservationButton = createNavButton("Reservation", loadReservation);
-  const contactButton = createNavButton("Contact", loadContact);
-
-  nav.appendChild(homeButton);
-  nav.appendChild(menuButton);
-  nav.appendChild(reservationButton);
-  nav.appendChild(contactButton);
-
-  return nav;
-}
-
-function createNavButton(text, onClickFunction) {
-  const button = document.createElement("button");
-  button.classList.add("button-nav");
-  button.textContent = text;
-  button.addEventListener("click", () => {
-    if (button.classList.contains("active")) return;
-    setActiveButton(button);
-    clearContent();
-    onClickFunction();
-  });
-  return button;
-}
-
-function setActiveButton(button) {
-  const buttons = document.querySelectorAll(".button-nav");
-
-  buttons.forEach((button) => {
-    if (button !== this) {
-      button.classList.remove("active");
-    }
-  });
-
-  button.classList.add("active");
-}
-
-function createMain() {
-  const main = document.createElement("main");
-  main.classList.add("main");
-  main.setAttribute("id", "main");
-  return main;
-}
+import loadHomePage from './home';
+import loadMenuPage from './menu';
+import loadReservationPage from './reservation';
+import loadContactPage from './contact';
 
 function clearContent() {
-  const content = document.getElementById("content");
-  content.innerHTML = "";
+  const content = document.getElementById('content');
+  while (content.firstChild) {
+    content.removeChild(content.firstChild);
+  }
 }
 
 function initializeWebsite() {
-  const content = document.getElementById("content");
-  
-  content.appendChild(createMain());
-  content.appendChild(createHeader());
-  setActiveButton(document.querySelector(".button-nav"));
-  loadHome();
+  const content = document.getElementById('content');
+  const navLinks = document.createElement('ul');
+  navLinks.classList.add("nav-link");
+  content.appendChild(navLinks);
+
+  const homeLink = document.createElement('li');
+  homeLink.classList.add("nav-link-child");
+  homeLink.textContent = "Home";
+  homeLink.addEventListener('click', () => {
+    clearContent();
+    loadHomePage();
+  });
+  navLinks.appendChild(homeLink);
+
+  const menuLink = document.createElement('li');
+  menuLink.classList.add("nav-link-child");
+  menuLink.textContent = "Menu";
+  menuLink.addEventListener('click', () => {
+    clearContent();
+    loadMenuPage();
+  });
+  navLinks.appendChild(menuLink);
+
+  const reservationLink = document.createElement('li');
+  reservationLink.classList.add("nav-link-child");
+  reservationLink.textContent = "Reservation";
+  reservationLink.addEventListener('click', () => {
+    clearContent();
+    loadReservationPage();
+  });
+  navLinks.appendChild(reservationLink);
+
+  const contactLink = document.createElement('li');
+  contactLink.classList.add("nav-link-child");
+  contactLink.textContent = "Contact";
+  contactLink.addEventListener('click', () => {
+    clearContent();
+    loadContactPage();
+  });
+  navLinks.appendChild(contactLink);
+
+  loadHomePage();
 }
+
+document.addEventListener('DOMContentLoaded', initializeWebsite);
 
 export default initializeWebsite;
