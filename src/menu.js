d1478500e8e67733c1c3a239d52d37f698f9b296
joshua nathaniel <ministerjoe456@gmail.com>
2023-10-07 03:22:50 +0100

    added a form for reservation

--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,57 @@
 function loadMenuPage() {
-    const content = document.getElementById('content');
-    const menuDiv = document.createElement('div');
-    menuDiv.textContent = 'Check out our delicious menu!';
-    content.appendChild(menuDiv);
-  }
-  
-  export default loadMenuPage;
-  
+  const content = document.getElementById('content');
+  content.innerHTML = '';
+
+  const menuDiv = document.createElement('div');
+  menuDiv.className = 'menu-page';
+
+  const menuItems = [
+    {
+      name: 'beans and plantain',
+      description: 'beans porrage, with plantain',
+      price: '$12.99',
+      imageSrc: 'src/assets/beans.jpg.jpeg',
+    },
+    {
+      name: 'Muschel spaghetti',
+      description: 'Classic spaghetti with tomato, muschel, and basil',
+      price: '$10.99',
+      imageSrc: 'src/assets/Muschel-Spaghetti.jpeg',
+    },
+    {
+      name: ' moi moi',
+      description: 'Freshly cooked grinded beans with egg',
+      price: '$14.99',
+      imageSrc: 'Nigerian_Moi-Moi.png',
+    },
+  ];
+
+  menuItems.forEach((item) => {
+    const menuItemDiv = document.createElement('div');
+    menuItemDiv.className = 'menu-item';
+
+    const itemImage = document.createElement('img');
+    itemImage.src = item.imageSrc;
+    itemImage.alt = item.name;
+
+    const itemName = document.createElement('h3');
+    itemName.textContent = item.name;
+
+    const itemDescription = document.createElement('p');
+    itemDescription.textContent = item.description;
+
+    const itemPrice = document.createElement('p');
+    itemPrice.textContent = item.price;
+
+    menuItemDiv.appendChild(itemImage);
+    menuItemDiv.appendChild(itemName);
+    menuItemDiv.appendChild(itemDescription);
+    menuItemDiv.appendChild(itemPrice);
+
+    menuDiv.appendChild(menuItemDiv);
+  });
+
+  content.appendChild(menuDiv);
+}
+
+export default loadMenuPage;
--- src/reservation.js
+++ src/reservation.js
@@ -1,63 +1,44 @@
-function loadResevervationPage() {
+function loadReservationPage() {
   const content = document.getElementById('content');
-  const reservationDiv = document.createElement('div');
-  reservationDiv.textContent = 'please fill out the form below to reserve a seat!';
 
-  
+  const reservationDiv = document.createElement('div');
+  reservationDiv.textContent = 'Please fill out the form below to make a restaurant reservation.';
+
   const reservationForm = document.createElement('form');
   reservationForm.id = 'reservationForm';
 
-  const nameLabel = document.createElement('label');
-  nameLabel.textContent = 'Name:';
-  const nameInput = document.createElement('input');
-  nameInput.type = 'text';
-  nameInput.name = 'name';
+  const formElements = [
+    { label: 'Name:', type: 'text', name: 'name' },
+    { label: 'Email:', type: 'email', name: 'email' },
+    { label: 'Phone:', type: 'tel', name: 'phone' },
+    { label: 'Number of Guests:', type: 'number', name: 'guests' }, 
+    { label: 'Date/Time:', type: 'datetime-local', name: 'dateTime' },
+  ];
 
-  const emailLabel = document.createElement('label');
-  emailLabel.textContent = 'Email:';
-  const emailInput = document.createElement('input');
-  emailInput.type = 'email';
-  emailInput.name = 'email';
+  formElements.forEach((element) => {
+    const label = document.createElement('label');
+    label.textContent = element.label;
 
-  const dateTimeLabel = document.createElement('label');
-  dateTimeLabel.textContent = 'Date/Time:';
-  const dateTimeInput = document.createElement('input');
-  dateTimeInput.type = 'datetime-local';
-  dateTimeInput.name = 'dateTime';
+    const input = document.createElement('input');
+    input.type = element.type;
+    input.name = element.name;
 
-  const seatLabel = document.createElement('label');
-  seatLabel.textContent = 'Seat:';
-  const seatSelect = document.createElement('select');
-  seatSelect.name = 'seat';
+    reservationForm.appendChild(label);
+    reservationForm.appendChild(input);
+  });
 
-  const option1 = document.createElement('option');
-  option1.value = 'table1';
-  option1.textContent = 'Table 1';
+  const specialRequestsLabel = document.createElement('label');
+  specialRequestsLabel.textContent = 'Special Requests:';
+  const specialRequestsTextarea = document.createElement('textarea');
+  specialRequestsTextarea.name = 'specialRequests';
 
-  const option2 = document.createElement('option');
-  option2.value = 'table2';
-  option2.textContent = 'Table 2';
-
-  const option3 = document.createElement('option');
-  option3.value = 'table3';
-  option3.textContent = 'Table 3';
-
-  seatSelect.appendChild(option1);
-  seatSelect.appendChild(option2);
-  seatSelect.appendChild(option3)
+  reservationForm.appendChild(specialRequestsLabel);
+  reservationForm.appendChild(specialRequestsTextarea);
 
   const submitButton = document.createElement('button');
   submitButton.type = 'submit';
-  submitButton.textContent = 'Book Now';
+  submitButton.textContent = 'Make Reservation';
 
-  reservationForm.appendChild(nameLabel);
-  reservationForm.appendChild(nameInput);
-  reservationForm.appendChild(emailLabel);
-  reservationForm.appendChild(emailInput);
-  reservationForm.appendChild(dateTimeLabel);
-  reservationForm.appendChild(dateTimeInput);
-  reservationForm.appendChild(seatLabel);
-  reservationForm.appendChild(seatSelect);
   reservationForm.appendChild(submitButton);
 
   reservationDiv.appendChild(reservationForm);
@@ -65,4 +46,4 @@ function loadResevervationPage() {
   content.appendChild(reservationDiv);
 }
 
-export default loadResevervationPage;
+export default loadReservationPage;
